1a74dea69fa7f01bfb3f419972b8d5467b27d6a0
lyakhliliana <91736225+lyakhliliana@users.noreply.github.com>
2021-10-01 15:23:29 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-students = []
-for i in range(n):
-    student, mark = input().split()
-    students.append((student, int(mark)))
-students.sort(key=lambda x: x[1], reverse=True)
-for i in range(n):
-    print(students[i][0])
